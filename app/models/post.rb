8e88da4cd3b36050f0afb62f5ab4fcb4d10a6ad3
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-22 13:02:04 -0500

    commit tree 8d7e39f2e7e17534015dc1e086508f6afd5a175b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /(Won't Believe|Secret|Top \d+|Guess)/
+      record.errors.add attribute, (options[:message] || "title not clickbait-y enough")
+    end
+  end
+end
+
 class Post < ApplicationRecord
+  validates :title, presence: true, title: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  
 end
